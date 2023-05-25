584098e404c1f08937028019eb92ddf16d2bd0e4
Yang <ruaruababa3333@gmail.com>
2023-05-25 23:24:49 +0700

    Doing write logic checkbox

--- src/services/role.ts
+++ src/services/role.ts
@@ -3,6 +3,11 @@ import {baseAPIRequest} from '../utils/service';
 export const getListRole = () => {
     return baseAPIRequest.get(`api/roles`);
 };
+
 export const getListPermissions = () => {
     return baseAPIRequest.get(`api/permissions`);
+};
+
+export const createRole = (input: any) => {
+    return baseAPIRequest.post(`api/roles`, input);
 };
