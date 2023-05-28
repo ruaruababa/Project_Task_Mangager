ed5bb028964dd72630bf30f199039a6eb55faa88
Yang <ruaruababa3333@gmail.com>
2023-05-28 22:22:01 +0700

    Upload search filter

--- src/components/DashboardUser/components/List/index.tsx
+++ src/components/DashboardUser/components/List/index.tsx
@@ -1,8 +1,8 @@
 import {useQuery} from '@tanstack/react-query';
-import {Button, Input} from 'antd';
+import {Button, Form, Input} from 'antd';
 import {useMemo, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import {getListUserInSystem} from '../../../../services/user';
+import {filterUser, getListUserInSystem} from '../../../../services/user';
 import Pagination from '../../../Pagination';
 import CreateUpdateUserModal from '../CreateUpdate/createUpdateUser';
 import UserItem from '../Item/item';
@@ -10,7 +10,6 @@ import UserItem from '../Item/item';
 const ListUser = () => {
     const [page, setPage] = useState(1);
     const [isShowCreateModal, setIsShowCreateModal] = useState(false);
-
     const {data: listUserResponse} = useQuery({
         queryKey: ['getListUserInSystem', page],
         queryFn: () => getListUserInSystem(page),
@@ -21,9 +20,22 @@ const ListUser = () => {
         return listUserResponse?.data?.data || [];
     }, [listUserResponse]);
 
+    const [userData, setUserData] = useState<any>(listUser);
+
     const total = useMemo(() => {
         return listUserResponse?.data?.meta?.total || 0;
     }, [listUserResponse]);
+
+    const [params, setParams] = useState<any>('');
+
+    const {data: userFilterResponse} = useQuery({
+        queryKey: ['filterUser', params],
+        queryFn: () => filterUser({...params}),
+    });
+
+    useMemo(() => {
+        setUserData(userFilterResponse?.data?.data || []);
+    }, [userFilterResponse]);
 
     const navigate = useNavigate();
 
@@ -43,20 +55,42 @@ const ListUser = () => {
                 </span>
             </div>
             <div className="flex flex-col p-10 bg-white rounded-lg">
-                <div className="flex justify-end gap-3 mb-10">
-                    <Input placeholder="Nhập người dùng" />
-                    <Button type="primary" size={'large'}>
-                        Tìm kiếm
-                    </Button>
-                    <Button
-                        className="text-white bg-blue-500"
-                        size={'large'}
-                        onClick={() => {
-                            setIsShowCreateModal(true);
+                <div className="flex justify-between gap-3 mb-10 ">
+                    <Form
+                        name="basic"
+                        onFinish={(values) => {
+                            setParams(values);
                         }}
+                        autoComplete="off"
                     >
-                        Thêm người dùng
-                    </Button>
+                        <div className="grid grid-cols-8 gap-3">
+                            <Form.Item name="userName" className="col-span-3">
+                                <Input placeholder="Nhập tên người dùng" />
+                            </Form.Item>
+
+                            <Form.Item name="email" className="col-span-3">
+                                <Input placeholder="Nhập email" />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Tìm kiếm
+                                </Button>
+                            </Form.Item>
+                        </div>
+                    </Form>
+                    <div>
+                        {' '}
+                        <Button
+                            className="text-white bg-blue-500"
+                            size={'large'}
+                            onClick={() => {
+                                setIsShowCreateModal(true);
+                            }}
+                        >
+                            Thêm người dùng
+                        </Button>
+                    </div>
                 </div>
                 <div className="flex flex-col text-xl">
                     <div className="grid grid-cols-8 pb-4 text-xs font-semibold text-gray-400 border-bottom">
@@ -67,7 +101,7 @@ const ListUser = () => {
                         <div className="">TRẠNG THÁI</div>
                         <div className="text-center">ACTION</div>
                     </div>
-                    {listUser.map((user: any) => {
+                    {userData?.map((user: any) => {
                         return (
                             <>
                                 <UserItem user={user} key={user?.name} />
@@ -79,7 +113,7 @@ const ListUser = () => {
             <div className="mt-5">
                 <Pagination
                     currentPage={page}
-                    totalCount={total}
+                    totalCount={total || userData?.length || 0}
                     pageSize={10}
                     onPageChange={(page: any) => setPage(page)}
                 />
