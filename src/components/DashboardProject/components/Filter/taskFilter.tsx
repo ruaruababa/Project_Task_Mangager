22a4c501283a2627baf0637ac2ee425988f3c40e
Yang <ruaruababa3333@gmail.com>
2023-05-31 23:39:02 +0700

    Add reset filter button, clear selected filters

--- src/components/DashboardProject/components/Filter/taskFilter.tsx
+++ src/components/DashboardProject/components/Filter/taskFilter.tsx
@@ -1,6 +1,17 @@
 import {Button, DatePicker, Form, Input, Select} from 'antd';
+import {useState} from 'react';
+import styled from 'styled-components';
 import useStatus from '../../../../hooks/useStatus';
 import useUser from '../../../../hooks/useUser';
+
+export const Container = styled.div<{toggleClearFiled: boolean}>`
+    .ant-select-clear {
+        opacity: ${({toggleClearFiled}) => (toggleClearFiled ? 1 : 0)};
+    }
+    .ant-picker-clear {
+        opacity: ${({toggleClearFiled}) => (toggleClearFiled ? 1 : 0)};
+    }
+`;
 interface Props {
     projectOtpions?: any;
     statusOptions?: any;
@@ -21,100 +32,128 @@ const FilterTask = (props: Props) => {
     };
 
     const {users} = useUser();
-
+    const [toggleClearField, setToggleClearField] = useState<any>(false);
     return (
-        <Form
-            form={form}
-            name="basic"
-            onFinish={(values) => {
-                setValues(values);
-            }}
-            autoComplete="off"
-        >
-            <div className="grid grid-cols-6 gap-3 mb-10">
-                <div className="col-span-1">
-                    {' '}
-                    <Form.Item name={'task_name'} className="!h-[40px]">
-                        <Input
-                            type="text"
-                            className="!h-[40px]"
-                            placeholder="Nhập tên task"
-                        ></Input>
+        <Container toggleClearFiled={toggleClearField}>
+            {' '}
+            <Form
+                form={form}
+                name="basic"
+                onFinish={(values) => {
+                    setValues(values);
+                }}
+                autoComplete="off"
+            >
+                <div className="grid grid-cols-6 gap-3 mb-10">
+                    <div className="col-span-1">
+                        {' '}
+                        <Form.Item name={'task_name'} className="!h-[40px]">
+                            <Input
+                                type="text"
+                                className="!h-[40px]"
+                                placeholder="Nhập tên task"
+                            ></Input>
+                        </Form.Item>
+                    </div>
+                    <Form.Item name={'user_do'} className="!h-[40px]">
+                        <Select
+                            placeholder="Chọn người thực hiện"
+                            fieldNames={{label: 'name', value: 'id'}}
+                            className="w-full"
+                            size={'large'}
+                            showSearch
+                            optionFilterProp="children"
+                            filterOption={(input: any, option: any) =>
+                                handleFilterOnChange(input, option)
+                            }
+                            filterSort={(optionA, optionB) =>
+                                handleFilterSort(optionA, optionB)
+                            }
+                            options={users}
+                            allowClear
+                            onChange={() => {
+                                setToggleClearField(true);
+                            }}
+                        />
                     </Form.Item>
-                </div>
-                <Form.Item name={'user_do'} className="!h-[40px]">
-                    <Select
-                        placeholder="Chọn người thực hiện"
-                        fieldNames={{label: 'name', value: 'id'}}
-                        className="w-full"
-                        size={'large'}
-                        showSearch
-                        optionFilterProp="children"
-                        filterOption={(input: any, option: any) =>
-                            handleFilterOnChange(input, option)
-                        }
-                        filterSort={(optionA, optionB) =>
-                            handleFilterSort(optionA, optionB)
-                        }
-                        options={users}
-                    />
-                </Form.Item>
-                <Form.Item name={'status_id'}>
-                    <Select
-                        placeholder="Chọn trạng thái"
-                        fieldNames={{label: 'label', value: 'value'}}
-                        className="w-full"
-                        size={'large'}
-                        showSearch
-                        optionFilterProp="children"
-                        filterOption={(input: any, option: any) =>
-                            handleFilterOnChange(input, option)
-                        }
-                        filterSort={(optionA, optionB) =>
-                            handleFilterSort(optionA, optionB)
-                        }
-                        options={statusOptions}
-                    />
-                </Form.Item>
-                <Form.Item
-                    name="start_at"
-                    valuePropName="startDate"
-                    className="!h-[40px]"
-                >
-                    <DatePicker
-                        className="!h-[40px]"
-                        placeholder="Ngày bắt đầu"
-                        onChange={() => {}}
-                        format={'YYYY/MM/DD HH:mm'}
-                        style={{
-                            width: '100%',
-                        }}
-                        showTime
-                    />
-                </Form.Item>
-                <Form.Item name="end_at" valuePropName="endDate">
-                    <DatePicker
-                        className="!h-[40px]"
-                        placeholder="Ngày kết thúc"
-                        onChange={() => {}}
-                        format={'YYYY/MM/DD HH:mm'}
-                        style={{
-                            width: '100%',
-                        }}
-                        showTime
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
+                    <Form.Item name={'status_id'}>
+                        <Select
+                            placeholder="Chọn trạng thái"
+                            fieldNames={{label: 'label', value: 'value'}}
+                            className="w-full"
+                            size={'large'}
+                            showSearch
+                            optionFilterProp="children"
+                            filterOption={(input: any, option: any) =>
+                                handleFilterOnChange(input, option)
+                            }
+                            filterSort={(optionA, optionB) =>
+                                handleFilterSort(optionA, optionB)
+                            }
+                            options={statusOptions}
+                            allowClear
+                            onChange={() => {
+                                setToggleClearField(true);
+                            }}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="start_at"
+                        valuePropName="startDate"
                         className="!h-[40px]"
                     >
-                        Tìm kiếm
-                    </Button>
-                </Form.Item>
-            </div>
-        </Form>
+                        <DatePicker
+                            className="!h-[40px]"
+                            placeholder="Ngày bắt đầu"
+                            format={'YYYY/MM/DD HH:mm'}
+                            style={{
+                                width: '100%',
+                            }}
+                            showTime
+                            allowClear
+                            onChange={() => {
+                                setToggleClearField(true);
+                            }}
+                        />
+                    </Form.Item>
+                    <Form.Item name="end_at" valuePropName="endDate">
+                        <DatePicker
+                            className="!h-[40px]"
+                            placeholder="Ngày kết thúc"
+                            format={'YYYY/MM/DD HH:mm'}
+                            style={{
+                                width: '100%',
+                            }}
+                            showTime
+                            allowClear
+                            onChange={() => {
+                                setToggleClearField(true);
+                            }}
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <div className="flex justify-evenly">
+                            {' '}
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                className="!h-[40px]"
+                            >
+                                Tìm kiếm
+                            </Button>
+                            <Button
+                                className="!h-[40px] bg-blue-500 text-white hover:bg-blue-600"
+                                onClick={() => {
+                                    form.resetFields();
+                                }}
+                            >
+                                Xóa bộ lọc
+                            </Button>
+                        </div>
+                    </Form.Item>
+                </div>
+            </Form>
+        </Container>
     );
 };
 
